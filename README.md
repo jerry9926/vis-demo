cbd4311158d50f97b19cdffe6bfde59162cc1bb3
kulerhuang <kulerhuang@local.com>
2018-03-09 17:53:22 +0800

    更新文件结构，增加生成index.html逻辑

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # vis-demo
 visjs
-1、mkdir dist
+1. mkdir dist
+2. npm script
+    - start 启动项目
+    - examples 查看vis 官方例子目录
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,30 @@
 /**
  * Created by zhijie.huang on 2017/3/7.
  */
-var gulp = require('gulp');
+const fs = require('fs'),
+    gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    reload = browserSync.reload;
 
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
+
+const ENTRY_PATH = 'src',
+    OUTPUT_PATH = 'dist',
+    INDEX_PATH = 'src/index.html';
 
 // 监视文件改动并重新载入
-gulp.task('serve', function() {
+gulp.task('serve', ['copy'], function() {
     console.log('task serve running');
 
     browserSync({
         server: {
-            baseDir: 'dist'
+            baseDir: OUTPUT_PATH
         }
     });
 
-    gulp.watch(['src/**', '!src/examples', '!src/examples/**'], ['reload']);
+    gulp.watch(['src/**', '!src/examples', '!src/examples/**'], ['reload'], function(cb) {
+        cb();
+    });
 });
 
 gulp.task('reload', ['copy'], function () {
@@ -27,18 +35,62 @@ gulp.task('default', ['serve'], function() {
     console.log('task default running');
 });
 
-gulp.task('default2', function() {
-    console.log('task default running');
+gulp.task('copy', ['clean', 'createIndex'], function() {
+    console.log('task copy running');
+    // 返回stream 以正确实现异步任务
+    return gulp.src(['src/**', '!src/examples', '!src/examples/**']).pipe(gulp.dest('dist'));
+});
 
-    var watcher = gulp.watch(['src/**', '!src/examples', '!src/examples/**'], ['copy']);
-    watcher.on('change', function(event) {
-        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+gulp.task('clean', function(cb) {
+    del(['dist/*.*','dist/**/*']).then(paths => {
+        console.log('Deleted files and folders:\n', paths.join('\n'));
+        cb();
     });
 });
 
-gulp.task('copy', function() {
-    var stream = gulp.src(['src/**', '!src/examples', '!src/examples/**']).pipe(gulp.dest('dist'));
-    // 返回stream 以正确实现异步任务
-    return stream;
+// 生成index.html
+gulp.task('createIndex', function(cb) {
+    return createIndex().then((data) => {
+        // console.log(data);
+    }).catch((err) => {
+        console.log('task createIndex error: ' + err);
+    })
 });
 
+function createIndex() {
+
+    function createIndexTemp() {
+        let links = [], num = 0;
+        const files = fs.readdirSync('src');
+
+        files.map((file) => {
+            if (file.match(/demo\d+\.html/)) {
+                num++;
+                links.push(`${num}. <a href="./${file}">${file}</a>`)
+            }
+        })
+        links = links.join('</br>');
+
+        return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>home page</title>
+        </head>
+        <body>
+            <h1>vis-demo homepage</h1>
+            ${links}
+        </body>
+        </html>`
+    }
+
+    return new Promise((resolve, reject) => {
+        const template = createIndexTemp();
+        fs.writeFile(INDEX_PATH, template, function(err) {
+            if (err) {
+                reject(err);
+            }
+            resolve('file saved success!');
+        })
+    })
+}
